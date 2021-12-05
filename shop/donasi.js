4fba2ad11a594098a6c3c8591f47d8fb0bcd0609
Irzan <79050885+Zaan322@users.noreply.github.com>
2021-12-05 21:51:49 +0800

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,14 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI BISA VIA E-MONEY
 
-*🏧 BANK*
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
 	
 *💰 E-MONEY*
 
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
+1. DANA
+	ㅁ 0853 4082 7717
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama H*******IN
 `
 	}
 
